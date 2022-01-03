aeb86a1dc89f00a6c3f0347a179181a49b58bd16
김명주 <rlaaudwn000@unionplace.co.kr>
2022-01-03 14:52:37 +0900

    vue 중급강의

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import News from '../views/News.vue';
+import Ask from '../views/Ask.vue';
+import Jobs from '../views/Jobs.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode: 'history',    // url 링크 해쉬(#) 값을 제거
+    routes: [
+        {
+            path: '/',
+            redirect: '/news'
+        },
+        {
+            path: '/news',
+            component: News,
+        },
+        {
+            path: '/ask',
+            component: Ask,
+        },
+        {
+            path: '/jobs',
+            component: Jobs,
+        }
+    ]
+})
