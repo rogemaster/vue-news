005cb3dda48213ad82a9d424a881e35606216344
rogemaster <rlaaudwn000@naver.com>
2022-03-04 23:17:08 +0900

    2022-03-04 news page 스타일링

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,18 @@ body {
   padding: 0;
   margin: 0;
 }
+
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+
+a.router-link-exact-active {
+  text-decoration: underline;
+}
 </style>
