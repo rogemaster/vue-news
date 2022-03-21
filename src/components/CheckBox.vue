8ad6f9581fb956a057c6bb60ef5f163aed1ee888
rogemaster <rlaaudwn000@naver.com>
2022-03-21 23:36:28 +0900

    2022-03-21 컴포넌트 디자인 패턴

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="checkbox" :value="value" @click="toggleCheck">
+</template>
+
+<script>
+export default {
+    // @input 이벤트
+    // :value 값
+    
+    props: ['value'],
+
+    methods: {
+        toggleCheck() {
+            this.$emit('input', !this.value);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
